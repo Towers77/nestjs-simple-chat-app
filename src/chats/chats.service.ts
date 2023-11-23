dff44e17b1bd8c98cff103fcb8ea083a736a116f
Torres <santitorres460@gmail.com>
2023-11-23 18:55:27 -0300

    Fix duplicate chat creation bug

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -19,6 +19,10 @@ export class ChatsService {
     const user1 = await this.usersService.findOneById(createChatDto.user1_id);
     const user2 = await this.usersService.findOneById(createChatDto.user2_id);
 
+    const chat = await this.chatRepository.find({ where: { user1, user2 } });
+
+    if (chat) return { message: 'Chat already exists' };
+
     await this.chatRepository.save({ user1, user2 });
 
     return { message: 'Chat created Succesfully' };
@@ -28,6 +32,15 @@ export class ChatsService {
     const user = await this.usersService.findOneById(id);
 
     return await this.chatRepository.find({
+      relations: {
+        user1: true,
+        user2: true,
+      },
+      select: {
+        id: true,
+        user1: { id: true, username: true },
+        user2: { id: true, username: true },
+      },
       where: [{ user1: user }, { user2: user, message_count: MoreThan(0) }],
     });
   }
