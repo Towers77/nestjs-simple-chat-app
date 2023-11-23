d75dd3d9fa0770842056937c56f03cbbbe13ab35
Torres <santitorres460@gmail.com>
2023-11-23 18:55:12 -0300

    Add access token guard to create and findAllByUserId methods

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 
 import { CreateChatDto } from './dto/create-chat.dto';
+import { AccessTokenGuard } from 'src/common/accessToken.guard';
 
 @Controller('chats')
 export class ChatsController {
   constructor(private readonly chatsService: ChatsService) {}
 
+  @UseGuards(AccessTokenGuard)
   @Post()
-  create(createChatDto: CreateChatDto) {
+  create(@Body() createChatDto: CreateChatDto) {
     return this.chatsService.create(createChatDto);
   }
 
+  @UseGuards(AccessTokenGuard)
   @Get(':user_id')
   findAllByUserId(@Param('user_id') user_id: number) {
     return this.chatsService.findAllByUserId(user_id);
