d91a5fd3b881837997c2c29416493679dd2a9028
Torres <santitorres460@gmail.com>
2023-11-29 01:17:26 -0300

    Added methods for handling messaging in service

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -3,9 +3,11 @@ import { MessagesService } from './messages.service';
 import { MessagesGateway } from './messages.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
+import { ChatsModule } from 'src/chats/chats.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message]), ChatsModule, UsersModule],
   providers: [MessagesGateway, MessagesService],
 })
 export class MessagesModule {}
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ChatsService } from 'src/chats/chats.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class MessagesService {
-  create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+  constructor(
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>,
+
+    private readonly chatsService: ChatsService,
+
+    private readonly usersService: UsersService,
+  ) {}
+
+  async create(createMessageDto: CreateMessageDto) {
+    const sent_by = await this.usersService.findOneById(
+      createMessageDto.sent_by,
+    );
+    const sent_in = await this.chatsService.findOneById(
+      createMessageDto.sent_in,
+    );
+
+    const message = await this.messageRepository.save({
+      text: createMessageDto.text,
+      sent_in,
+      sent_by,
+    });
+    await this.chatsService.updateMessageCount(sent_in.id);
+
+    const response = {
+      id: message.id,
+      text: message.text,
+      created_at: message.created_at,
+      sent_by: {
+        id: sent_by.id,
+        username: sent_by.username,
+      },
+    };
+    return response;
   }
 
-  findAll() {
-    return `This action returns all messages`;
-  }
+  async findAllFromChat(chatId: number) {
+    const chat = await this.chatsService.findOneById(chatId);
 
-  findOne(id: number) {
-    return `This action returns a #${id} message`;
-  }
-
-  update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+    return await this.messageRepository.find({
+      relations: { sent_by: true },
+      select: {
+        id: true,
+        created_at: true,
+        text: true,
+        sent_by: { id: true, username: true },
+      },
+      where: { sent_in: chat },
+    });
   }
 }
